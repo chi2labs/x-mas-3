019b107259fa0b7e7f3d2b61f452ada6a8a424ac
Aleksander Dietrichson <dietrichson@gmail.com>
2023-08-02 13:56:18 +0000

    Updated Agent 0 to make only legal moves

--- R/agent-0.R
+++ R/agent-0.R
@@ -6,9 +6,22 @@
 #' @export
 
 agent_0 <-\(B){
-  my_move <- sample(c("U","D","L","R"),1)
+   possible_moves <- c("U","D","L","R")
+   cm <-c()
   .c <- sample(1:10,1)
   .r <- sample(1:6,1)
+  # Make sure move is legal
+  if(.c == 1 ){cm <- c(cm("U"))}
+  if(.c == 10 ){cm <- c(cm("D"))}
+  if(.r == 1)(cm <- c(cm("L")))
+  if(.r == 10)(cm <- c(cm("R")))
+  
+  #Update Possible moves
+  possible_moves <- possible_moves[!possible_moves%in%cm]
+  
+  my_move <- sample(possible_moves,1)
+  
+  
   list(
     Board = move(B,.r,.c,my_move),
     metadata = data.frame(col=.c,row=.r,move=my_move)
