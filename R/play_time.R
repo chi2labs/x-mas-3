bc9406b9d9971d8c2ecd64d1017145c95ad40f23
Aleksander Dietrichson <Aleksander.Dietrichson@blackboard.com>
2023-08-03 21:06:23 -0300

    adding updates to robot

---
+++ R/play_time.R
@@ -0,0 +1,4 @@
+remDr$mouseMoveToLocation(x = 600,y = 370 ,game_canvas[[1]])
+remDr$click()
+Sys.sleep(.3)
+robot_play_game(max_moves=1500)
--- R/robot-play.R
+++ R/robot-play.R
@@ -35,6 +35,8 @@ robot_initialize <- function(){
   remDr$open()
   remDr$setWindowSize(width = 1200,height = 831)
   #remDr$setWindowSize(1600,868)
+
+  #remDr$maxWindowSize()
   message("Loading Game...")
   remDr$navigate("https://gato-files-prod.s3.amazonaws.com/assets/games/X-MAS%203/index.html")
   
