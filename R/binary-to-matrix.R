99f81b92e318b04b83f1306155d814f1a3b557fd
Aleksander Dietrichson <dietrichson@gmail.com>
2023-08-08 15:15:09 +0000

    added function

---
+++ R/binary-to-matrix.R
@@ -0,0 +1,12 @@
+#' Convert binary string back to matrix
+#'
+#' @param x 
+#'
+#' @return matrix
+#' @export
+binary_to_matrix <- function(x){
+  tmp <- stringr::str_split(x,"",simplify = TRUE) |> as.integer()
+  M <- matrix(tmp, ncol = 4, nrow = 5)
+  colnames(M) <- LETTERS[1:4]
+  M
+}
