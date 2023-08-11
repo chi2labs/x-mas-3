6d2813230027dfd13c5414772256d426280d5ac9
Aleksander Dietrichson <dietrichson@gmail.com>
2023-08-11 02:34:17 +0000

    Added function to evaluate from LUT instead of the model. Performance

--- R/evaluate_board.R
+++ R/evaluate_board.R
@@ -1,5 +1,5 @@
 
-.calculate_expected_values <-  function(model,S, dims=c(3,5)){
+.calculate_expected_values <-  function(q_model,S, dims=c(3,5)){
   max_moves <-max(dims) %>% unique()
   purrr::map(S,~{
        m <-0
@@ -8,10 +8,11 @@
        move_seq <- ""
        while (score==0 & m <= max_moves) {
           m <- m+1
-          my_move <- predict(model,B %>% as.character(.x))
+          my_move <- predict(q_model,B %>% as.character(.x))
           move_seq <-c(move_seq,my_move) 
           B <-make_move(B,my_move)
           score <- score_binary_board(B)
+          if(my_move=="Pass") break
        }
          data.frame(score =score, 
                     expected_value = score/m, 
@@ -50,6 +51,44 @@ get_matrix_quadrants <- function(M,dims){
   
 }
 
+
+
+#' Evaluate Board Using Lookup Table
+#' 
+#' LUT is available and speeds up the evaluation process
+#'
+#' @param B Board to evaluate
+#' @param LUT Lookup Table
+#' @param dims dimension on model
+#'
+#' @return data.frame with suggested moves etc.
+#' @importFrom dplyr left_join
+#' @export
+evaluate_board_LUT <- function(B,LUT = Gatai_LUT, dims=c(3,5)){
+  W <- get_matrix_quadrants(B,dims)
+  tile_types <- unique(B %>% as.character() %>% sort())
+  res <- data.frame()
+  Q=0
+  for(w in W){
+    Q <- Q+1
+    pos <- attr(w,'pos')
+    for(tile in tile_types){
+      State <-
+        ifelse(tile==w,1,0) %>% 
+        bin2int() 
+      res <- rbind(
+        res, data.frame(Q=Q,Row=pos[1],Column=pos[2],Tile=tile,State)
+      )
+      
+    }
+  }
+  res |> 
+    left_join(LUT |> mutate(State=as.numeric(State)))
+    
+  
+  
+}
+
 #' Evaluate a board based in a Model
 #' 
 #' The function will create sequential windows on the board, and pass these through the model.
@@ -63,7 +102,7 @@ get_matrix_quadrants <- function(M,dims){
 #' @importFrom adana bin2int
 #' @return data.frame with suggested move and expected score
 #' @export
-evaluate_board <- function(B,model, dims=c(3,5),include_strategy = TRUE){
+evaluate_board <- function(B,q_model, dims=c(3,5),include_strategy = TRUE){
   W <- get_matrix_quadrants(B,dims)
   tile_types <- unique(B %>% as.character() %>% sort())
   res <- data.frame()
@@ -81,13 +120,29 @@ evaluate_board <- function(B,model, dims=c(3,5),include_strategy = TRUE){
       
     }
   }
-  res$nextMove <- predict(model,res$State)
+  #res$nextMove <- predict(q_model,res$State)
   #my_expected <- .calculate_expected_values(res$State,dims)
-  res <- filter(res,(nextMove !="Pass"))
+  #res <- filter(res,(nextMove !="Pass"))
   if(include_strategy){
-    expected_value <- .calculate_expected_values(model,res$State)
+    expected_value <- .calculate_expected_values(q_model,res$State)
   res <-  cbind(res,expected_value)
   }
   res
 }
+
+
+# if(!exists("gatai")){
+#   gatai <- readr::read_rds(here::here("models/q-learning-3x5.rds"))
+# }
+# M <- initialize_board()
+
+# system.time(
+#   E <- evaluate_board(M,q_model=gatai,dims = c(3,5))
+# ) |> print()
+# Gatai_LUT <- readr::read_rds("inst/LUTS/LUT-3x5.rds")
+# system.time(
+#   E2 <- evaluate_board_LUT(M,LUT=Gatai_LUT,dims = c(3,5))
+# ) |> print()
+
+
 
