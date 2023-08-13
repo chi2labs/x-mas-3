44780efa4943ffc8d57cb0c2d959febe51736045
Aleksander Dietrichson <dietrichson@gmail.com>
2023-08-13 13:42:35 +0000

    Updated agents

--- R/agent-4.R
+++ R/agent-4.R
@@ -1,19 +1,20 @@
-#' Agent 5
+#' Agent 4
 #' 
-#' Plays a 5x3 Quarter Strategy
+#' Plays a 4x4 Grid 
 #'
 #' @param B Board Matrix
 #' @param ... not in use for this agent
 #' @import dplyr
 #' @return a data.frame containing as least move_sequence 
 #' @export
-agent_5 <-function(B, ...){
+agent_4 <-function(B, ...){
   Q <- tibble(
-    rows = c(1,4,1,4),
-    cols = c(1,1,5,5)
+    rows = c(1,3,1,3,2),
+    cols = c(1,1,6,6,3)
   )
+  
   moves <- purrr::map2(Q$rows,Q$cols,~{
-    b <- get_sub_board(B,from = c(.x,.y), dims=c(3,5))
+    b <- get_sub_board(B,from = c(.x,.y), dims=c(4, 4))
     evaluate_grid(b) %>% 
       mutate(Row = .x, Column = .y) %>% 
       filter(sequence !="Pass") 
@@ -34,15 +35,20 @@ agent_5 <-function(B, ...){
     
   }
   if(nrow(moves)==0){
-    message("Agent 5 found no playable moves, trying Agent 4")
-    moves <- agent_4(B)
+    message("Agent 4 found no playable moves, trying Agent 3")
+    #moves <- agent_3(B)
+    message("Agent 3 is on vacation maybe Agent 0 can help.")
+    moves <- agent_0(B,n=1)
   }
   moves
 }
 
-if(interactive()){
-  set.seed(1234)
-  B <- initialize_board()
-  agent_5(B) %>% print()
-  
-}
+# if(interactive()){
+#   system.time({
+#     
+#   set.seed(1234)
+#   B <- initialize_board()
+#   agent_4(B) %>% print()
+#   
+#   }) |> print()
+# }
