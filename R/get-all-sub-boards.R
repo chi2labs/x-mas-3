b2c9bc97dfdae8c4fab90a439f06c1da224676f3
Aleksander Dietrichson <Aleksander.Dietrichson@blackboard.com>
2023-08-26 12:08:30 -0300

    Disambiguated parameter names

--- R/get-all-sub-boards.R
+++ R/get-all-sub-boards.R
@@ -8,12 +8,12 @@
 #' @return a list with the sub-boards
 #' @export
 get_all_sub_boards <- function(B, geometry="3x3"){
-  dims <- parse_geometry(geometry)
-  row_range <- 1:((dim(B)[1]-dims[1])+1)
-  col_range <- 1:((dim(B)[2]-dims[2])+1)
+  my_dims <- parse_geometry(geometry)
+  row_range <- 1:((dim(B)[1]-my_dims[1])+1)
+  col_range <- 1:((dim(B)[2]-my_dims[2])+1)
   purrr::map(row_range,function(.r){
     purrr::map(col_range,function(.c){
-      get_sub_board(B,c(.r,.c),dims)
+      get_sub_board(B,c(.r,.c),dims=my_dims)
     })
   })
 }
